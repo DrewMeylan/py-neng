f9caa6014a4fe2ba69d116daffac33423ea346a0
KidScirpto <kidscripto@proton.me>
2023-05-02 09:47:12 -0400

    .

--- simple_ping.py
+++ simple_ping.py
@@ -5,9 +5,17 @@ Date:
 '''
 
 import subprocess
+import argparse
 
-def ping_ip(ipAddr):
-    reply = subprocess.run(['ping', '-c', '3', '-n', ipAddr],
+#Argument Definition
+parser = argparse.ArgumentParser(description="Define IP address, ping count")
+parser.add_argument('-i', '--ipAddr', help='Target IP address', type=str, required=True)
+parser.add_argument('-c', '--count', help='Count number', type=int, required=True)
+args = parser.parse_args()
+
+
+def ping_ip():
+    reply = subprocess.run(['ping', '-c', str(args.count), '-n', str(args.ipAddr)],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            encoding='utf-8')
@@ -18,5 +26,5 @@ def ping_ip(ipAddr):
         print(False)
 
 
-## TEST
-#ping_ip('8.8.8.8')  
+if __name__ == "__main__":
+    ping_ip()
