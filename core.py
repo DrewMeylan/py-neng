cd90fd98a92cdf12178acba49796ac03b8de00e8
Drew <kidscripto@proton.me>
2023-08-02 09:28:30 -0400

    updated venv

--- core.py
+++ core.py
@@ -1,13 +1,23 @@
 ### Importations
-
+from netmiko import ConnectHandler
 ### 
-
+CSR = {
+    "device_type": "cisco_ios", 
+    "ip": "sandbox-nso-1.cisco.com",
+    "username":"developer",
+    "password":"Services4Ever"
+}
 # Functions definition
+def main():
+    net_connect = ConnectHandler(**CSR)
+    output = net_connect.send_command('show ip interface brief')
+    print(output)
+    net_connect.disconnect()
 
 # Classes definition
 
-def main()
-    print("Hello world")
+#def main()
+ #   print("Hello world")
 
 
 
