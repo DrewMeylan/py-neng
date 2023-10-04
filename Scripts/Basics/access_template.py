1adccf1fe6ec40dab6724f10f2af98ba49901ccd
drew <drew@pop-os.localdomain>
2023-10-04 17:33:18 -0400

    .

--- Scripts/Basics/access_template.py
+++ Scripts/Basics/access_template.py
@@ -1,15 +1,8 @@
 #! /usr/bin/env python3
-## sys args 
-from sys import argv
 import argparse
 from netmiko import ConnectHandler
 
-ip = argv[1]
-interface = argv[2]
-template = argv[3]
-vlans = argv[4]
-
-## Argument Definitions using ArgParse?
+## Argument Definitions w/ Argparse
 parser = argparse.ArgumentParser(description="Get Hostname")
 parser.add_argument('-n', '--hostname', required=True, type=str)
 parser.add_argument('-i', '--ip', required=False, type=str) #False if DNS resolution is possible
@@ -18,17 +11,8 @@ parser.add_argument('-v', '--vlan(s)', required=True, type=str)
 parser.add_argument('-t', '--type', required=True, default='cisco_ios', type=str)  
 args = parser.parse_args()
 
-'''
-device_settings {
-    "device_type": "cisco_ios",
-    "ip"
-}
-device = {**deivce}
-'''
-
-## Access Template template definition
-###Class permutations? IDK
-use = [
+## Define Used vs Unusued Templates
+used_template = [
     'swtichport mode access',
     f'switchport access vlan {arg2}',
     'switchport nonegotiate',
@@ -36,14 +20,12 @@ use = [
     'spanning-tree bpduguard enable'
 ]
 
-unuse = [
+unused_template = [
     'switchport mode access',  #Manually define access mode
     f'switchport access vlan {SOMETHING}', #Move to unusued vlan
     'switchport nonegotiate', #Disable DTP
     'shut' #Shutdown interface
 ]
-
-net_connect = ConnectHandler(**ip)
 
 def applyTemplate(ip, interface, template, vlans):
     device_info = {
@@ -59,12 +41,26 @@ def applyTemplate(ip, interface, template, vlans):
     for cmd in template:
         net_connect.send_command(cmd)
         
-
-
-
 ## Push template config def to target(s)
 print('\n'.join(access_template).format(5))
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':.
+# Define device information from provided paramters
+device_info {
+    "device_type": f"{type}",
+    "ip": f"{ip}",
+    "username": f"{username}",
+    "password": f"{password}"
+}
+# Define create device object; this part seems redundant
+device = {**device_info}
+
+# Create connectHandler object
+net_connect = ConnectHandler(**device)
+
+# Using net_connect object, send commands to device
+
+
+
 
