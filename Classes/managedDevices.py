b6a2cfab6890a6cf28de8d76178d6460a80c13c6
drew <drew@pop-os.localdomain>
2023-10-05 14:26:26 -0400

    .

--- Classes/managedDevices.py
+++ Classes/managedDevices.py
@@ -1,4 +1,9 @@
-import netmiko 
+from netmiko import ConnectHandler
+
+class IterRegistry(type):
+    def __iter_(cls):
+        return iter(cls._registry)
+
 
 class managedDevice:
     def __init__(self, hostname, vendor, devtype, devmodel, management_IP):
@@ -7,9 +12,28 @@ class managedDevice:
         self.type = devtype
         self.model = devmodel
         self.management_IP = management_IP
+    
+    def device_info(self):
+        return {
+            'device_type': self.vendor,
+            'hostname': self.hostname,
+            'username': 
+            'password': 
+        }
 
+    def _str__(self):
+        return {
+            'device_type': self.hostname
 
-'''
-    def show(self):
-        print('Name:', self.name, "\nManagement IP:", self.management_IP)
-'''     
+        }
+
+class switch(managedDevice):
+    def __init__(self, hostname):
+    super().__init__    
+    pass
+
+class router(managedDevice):
+    pass
+
+class endpoint(managedDevice):
+    pass
