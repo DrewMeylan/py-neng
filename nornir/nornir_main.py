73b60a0c9c0cb3e2bd5c3539f5df955bbe3a1d73
drew <kidscripto@proton.me>
2023-10-08 23:23:11 +0000

    .

--- nornir/nornir_main.py
+++ nornir/nornir_main.py
@@ -3,7 +3,18 @@ from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
 
 nr = InitNornir(
-        config_file="config.yaml", dry_run=True
+        runner={
+            "plugin": "threaded",
+            "options": {
+                "num_workers": 20,
+                },
+            },
+        inventory={
+            "plugin": "SimpleInventory",
+            "options": {
+                "host_file": "/home/kidscripto/Git/py-neng/nornir/inventory/hosts.yaml"
+                },
+            },
         )
 
 results = nr.run(task=napalm_get, getters=["facts"])
