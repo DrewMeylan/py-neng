a02ee40717919b1afb520c9140300941c9efc6ce
Drew <kidscripto@proton.me>
2023-10-09 21:32:40 -0400

    .

--- Scripts/dns_resolver.py
+++ Scripts/dns_resolver.py
@@ -1,29 +1,31 @@
 from nornir import InitNornir
-import sockets
+import socket
 import re
 
 '''
 Query DNS func
 !! Maybe recode to take in different types of input?
 '''
-
 def query_dns(hostpath) -> dict:
     ''' Takes in hosts file and outputs a dict with each hostname and corresponding IP'''
     dns_results = {} 
     with open(hostpath) as hosts:
         for host in hosts:
-            response = socket.gethostbyname_ex(host)
-            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', response)
-            dns_resultsresults[host] = ip
+            try:
+                response = socket.gethostbyname_ex(host)
+                ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(response))
+                dns_results[host] = ip
+            except:
+                dns_results[host] = "Null"
     return dns_results
 
 '''
 Compare DNS func
 '''
 
-@query_dns
-def compare_dns() -> dict:
-    ''' Takes dict of hostname:IP and compares against NetBox'''
+#@query_dns
+#def compare_dns() -> dict:
+#    ''' Takes dict of hostname:IP and compares against NetBox'''
 
 if __name__ == "__main__":
     query_dns()
