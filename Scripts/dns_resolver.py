6a60c74cd45ad3871310e337e2b88a0d26756517
Drew <kidscripto@proton.me>
2023-10-10 21:03:25 -0400

    .

--- Scripts/dns_resolver.py
+++ Scripts/dns_resolver.py
@@ -1,33 +1,65 @@
-from nornir import InitNornir
-import socket
-import re
+#from nornir import InitNornir
+from re import re.compile, re.findall
+from socket import gethostbyname_ex as gethost
+import sys
 
 '''
-Query DNS func
-!! Maybe recode to take in different types of input?
+Pattern def and sifters
 '''
-def query_dns(hostpath) -> dict:
-    ''' Takes in hosts file and outputs a dict with each hostname and corresponding IP'''
+ip_pattern = re.compile(r'\d+\.\d+\.\d+\.\d+')
+
+'''
+Decorators for extensibility: handle different inputs types to construct list for Query function
+'''
+def sysargs(func):
+    def sysarg_query(*args, *kwargs, sys.argv):
+        func(sys.argv) ## sys.argv is of type list; can be passed straight through to decorated query function
+    return sysarg_query
+
+def filepath(func, sifter=None):  ## Take in the query function and a sifter, which can be used to filter the 
+    def file_query(*args, **kwargs, filepath):
+        hostlist = []
+        with open(filepath) as hosts:
+            for host in hosts:
+                hostlist.append(str(host).rstrip())
+        func(hostlist)
+    return file_query
+
+'''
+Query Function --> Construct dictionary from hostname list to compare against netbox
+'''
+def query(host:list) -> dict:
+    ''' Takes in a list of hostnames outputs a dict with each hostname and corresponding IP'''
     dns_results = {} 
-    with open(hostpath) as hosts:
+    with open(*args) as hosts:  ### Export file context management to decorator
         for host in hosts:
+            host = host.rstrip()  ## rstrip should become redundant with decorators
             try:
-                response = socket.gethostbyname_ex(host)
-                ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(response))
+                response = str(socket.gethostbyname_ex(host))
+                ip = re.findall(ip_pattern, response)
                 dns_results[host] = ip
             except:
                 dns_results[host] = "Null"
     return dns_results
 
-'''
-Compare DNS func
-'''
 
-TEST!!!
-
+'''
+Compare Function --> Checks dictionary values against netbox and returns an array: [Hostname], [DNS VAL], [Netbox VAL], [Bool ( 1 = Diff ) ]
+'''
 #@query_dns
 #def compare_dns() -> dict:
 #    ''' Takes dict of hostname:IP and compares against NetBox'''
 
+'''
+
+'''
+
+def main():
+    if len(sys.argv >= 1):
+        sysarg_query(sys.argv)
+        pass
+    if 
+
 if __name__ == "__main__":
-    query_dns()
+    main()
+    exit(1)
