bf9789e9843989690eb49123224469dae706171e
drew <drew@pop-os.localdomain>
2023-10-11 17:23:53 -0400

    .

--- Scripts/dns_resolver.py
+++ Scripts/dns_resolver.py
@@ -4,26 +4,45 @@ from socket import gethostbyname_ex as gethost
 import sys
 
 '''
-Pattern def and sifters
+Pattern def and sifter standalone func
 '''
 ip_pattern = re.compile(r'\d+\.\d+\.\d+\.\d+')
+
+def sifter(pattern):  ## Takes in a string and generates a function to serve as a regex filter to match the passed string
+    ''' Sifter function takes in a regex pattern and returns filter_text function  '''
+    try:
+        regex = re.compile(pattern)
+        def filter_text(text):
+            return regex.findall(text)
+        return filter_text
+    except Exception as error:
+        print(f'Error parsing string: {error}')
+        return None
 
 '''
 Decorators for extensibility: handle different inputs types to construct list for Query function
 '''
-def sysargs(func):
-    def sysarg_query(*args, *kwargs, sys.argv):
-        func(sys.argv) ## sys.argv is of type list; can be passed straight through to decorated query function
-    return sysarg_query
+def sysargs(func): ### Take in sysargs and pass them directly to the decorated query function as a list
+    def sysarg_query(*args) -> list:
+        '''Sysarg_query takes in system arguments as a list and passes this list to the decorated query function'''
+        func(*args) ## sys.argv is of type list; can be passed straight through to decorated query function
+    return sysarg_query # Returns function, not a list?
 
-def filepath(func, sifter=None):  ## Take in the query function and a sifter, which can be used to filter the 
-    def file_query(*args, **kwargs, filepath):
+def filepath(func, sift_string:str=None):  ## Take in the query function and a sifter, which can be used to filter the 
+    def file_query(filepath:str) -> list:
         hostlist = []
         with open(filepath) as hosts:
             for host in hosts:
                 hostlist.append(str(host).rstrip())
-        func(hostlist)
+            
+            if sift_string:
+                inner_filter = sifter(sift_string)
+                filtered_hosts = inner_filter(" ".join(hostlist))
+                func(filtered_hosts)
+            else:
+                func(hostlist)
     return file_query
+
 
 '''
 Query Function --> Construct dictionary from hostname list to compare against netbox
@@ -56,6 +75,7 @@ Compare Function --> Checks dictionary values against netbox and returns an arra
 
 def main():
     if len(sys.argv >= 1):
+        args = sys.argv
         sysarg_query(sys.argv)
         pass
     if 
--- Scripts/test.py
+++ Scripts/test.py
@@ -1,3 +1,27 @@
 import sys
+import socket
+import re
 
-print(type(sys.argv))
+args = sys.argv
+ip_pattern = re.compile(r'\d+\.\d+\.\d+\.\d+')
+'''
+def sysargs(func):
+    def sysarg_query(*args) -> list:
+        'This is a sysarg_query docstring'
+        func(*args)
+    return sysarg_query 
+'''
+def query(some_fucking_list):
+    dns_results = {}
+    for host in some_fucking_list:
+        try:
+            response = str(socket.gethostbyname_ex(host))
+            ip = re.findall(ip_pattern, response)
+            dns_results[host] = ip
+        except:
+            dns_results[host] = "Null"
+    return dns_results
+
+
+if __name__ == '__main__':
+    print(query([args]))
