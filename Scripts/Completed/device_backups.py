9a74ab3a85ca7501ae16a050ccdf04eef43a447e
drew <kidscripto@proton.me>
2023-10-23 16:58:32 -0400

    .

--- Scripts/Completed/device_backups.py
+++ Scripts/Completed/device_backups.py
@@ -1,29 +1,37 @@
 from netmiko import ConnectHandler
+import getpass
 
-with open('devices.txt') as routers:
-    for IP in routers:
-        router = {
-                'device_type': 'cisco_ios',
-                'ip': IP,
-                'username': 'roger',
-                'password': 'cisco'
-        }
+username = input("Enter username: ")
+passwd = getpass.getpass("\nEnter password: ")
 
-        net_connect = ConnectHandler(**router)
-        hostname = net_connect.send_command('sh run | i host')
-        hostname.split(" ")
-        hostname, device = hostname.split(" ")
-        print("Backing up " + device + "...")
+def backup_devices(hostfile, filter=None):
+    username = input("Enter username: ")
+    passwd = getpass.getpass("\nEnter Password: ")
 
-        filename = '/home/drew/backups/network_devices/' + device + '.txt'
+    with open(hostfile, "r") as routers:
+        for IP in routers:
+            router = {
+                    'device_type': 'cisco_ios',
+                    'ip': IP,
+                    'username': username,
+                    'password': passwd
+            }
+
+            net_connect = ConnectHandler(**router)
+            hostname = net_connect.send_command('sh run | i host')
+            hostname.split(" ")
+            hostname, device = hostname.split(" ")
+            print("Backing up " + device + "...")
+
+            filename = '/home/drew/backups/network_devices/' + device + '.txt'
         
-        showrun = net_connect.send_command('show run')
-        showvlan = net_connect.send_command('show vlan')
-        showver = net_connect.send_command('show ver')
-        with open(filename, "a") as log_file:
-            log_file.write(showrun + "\n")
-            log_file.write(showvlan + "\n")
-            log_file.write(showver + "\n")
+            showrun = net_connect.send_command('show run')
+            showvlan = net_connect.send_command('show vlan')
+            showver = net_connect.send_command('show ver')
+            with open(filename, "a") as log_file:
+                log_file.write(showrun + "\n")
+                log_file.write(showvlan + "\n")
+                log_file.write(showver + "\n")
 
-net_connect.disconnect()
+    net_connect.disconnect()
         
